10347d39df5c8fdbfffc18eeb8781d0cf3f2b440
RIDHIJ-19 <128258454+RIDHIJ-19@users.noreply.github.com>
2023-03-25 13:01:29 +0530

    Add files via upload

---
+++ APPLI.py
@@ -0,0 +1,87 @@
+import pygame,sys
+from pygame.locals import *
+import numpy as np
+from keras.models import load_model
+import cv2
+
+
+WINDOWSIZEX = 640
+XINDOWSIZEY = 480
+BOUNDRYINC = 5
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+IMAGESAVE = False
+
+MODEL = load_model("bestmodel.h5")
+
+LABELS = {0: "Zero", 1: "One", 
+          2: "Two", 3: "Three", 
+          4: "Four", 5: "Five",
+          6: "Six", 7: "Seven",
+          8: "Eight", 9: "Nine"}
+
+pygame.init()
+pygame.font.get_init()
+
+FONT = pygame.font.SysFont('freesansbold.ttf', 18)
+
+# Create Pygame window
+DISPLAYSURF = pygame.display.set_mode((WINDOWSIZEX, XINDOWSIZEY))
+pygame.display.set_caption("DigiScan")
+
+number_xcord = []
+number_ycord = []
+iswriting = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == MOUSEMOTION and iswriting:
+            xcord, ycord = event.pos
+            pygame.draw.circle(DISPLAYSURF, WHITE, (xcord, ycord), 4, 0)
+            number_xcord.append(xcord)
+            number_ycord.append(ycord)
+
+        if event.type == MOUSEBUTTONDOWN:
+            iswriting = True
+
+        if event.type == MOUSEBUTTONUP:
+            iswriting = False
+            number_xcord = sorted(number_xcord)
+            number_ycord = sorted(number_ycord)
+
+            rect_min_x, rect_max_x = max(number_xcord[0] - BOUNDRYINC, 0), min(WINDOWSIZEX, number_xcord[-1] + BOUNDRYINC)
+            rect_min_y, rect_max_y = max(number_ycord[0] - BOUNDRYINC, 0), min(XINDOWSIZEY, number_ycord[-1] + BOUNDRYINC)
+
+            number_xcord = []
+            number_ycord = []
+
+            # Get pixel array from Pygame window surface
+            img_arr = np.array(pygame.PixelArray(DISPLAYSURF))[rect_min_x:rect_max_x, rect_min_y:rect_max_y].T.astype(np.float32)
+
+            if IMAGESAVE:
+                cv2.imwrite("image.png")
+                image_cnt += 1
+
+            image = cv2.resize(img_arr, (28, 28))
+            image = np.pad(image, (10, 10), 'constant', constant_values=0)
+            image = cv2.resize(image, (28, 28)) / 255
+
+            label = str(LABELS[np.argmax(MODEL.predict(image.reshape(1, 28, 28, 1)))])
+                
+            textSurface = FONT.render(label, True, RED, WHITE)
+            textRecObj = textSurface.get_rect()
+            textRecObj.left, textRecObj.bottom = rect_min_x, rect_max_y - 10
+                
+            DISPLAYSURF.blit(textSurface, textRecObj)
+
+        if event.type == KEYDOWN:
+            if event.unicode == "n":
+                DISPLAYSURF.fill("#000036")
+
+    pygame.display.update()
